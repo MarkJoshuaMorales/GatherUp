8cb2761a15b739d057f861e0821608205c9d332d
Mark Morales <mjlmorales2021@plm.edu.ph>
2024-12-04 22:51:54 +0800

    Few revisions such as making texts bold

--- Home/static/js/eventcreation.js
+++ Home/static/js/eventcreation.js
@@ -1,34 +1,3 @@
-document.getElementById('location-selector').addEventListener('change', function handleLocationChange() {
-    const container = document.querySelector('.location-container');
-    const selectorValue = this.value;
-
-    if (selectorValue === 'offline') {
-        
-        container.innerHTML = `
-            <input type="text" class="location-input" placeholder="Enter On-site Address">
-        `;
-    } else if (selectorValue === 'online') {
-        
-        container.innerHTML = `
-            <input type="text" class="location-input" placeholder="Enter Online Link">
-        `;
-    } else {
-        
-        container.innerHTML = `
-            <select class="location-type" id="location-selector">
-                <option value="online">Online Link</option>
-                <option value="offline">On-site Address</option>
-            </select>
-        `;
-
-        
-        const newSelector = document.getElementById('location-selector');
-        if (newSelector) {
-            newSelector.addEventListener('change', handleLocationChange);
-        }
-    }
-});
-
 document.getElementById('ticket-input').addEventListener('input', function () {
     const ticketPrice = parseFloat(this.value);
     const ticketType = ticketPrice === 0 ? "Free" : "Paid";
